8443cdc3e1fd3f9eff1ab88fe5940f499e927558
BlaBlahBlahh <rithunyaa@gmail.com>
2026-09-05 23:53:27 +0000

    added a few situations

--- game.py
+++ game.py
@@ -11,3 +11,26 @@ print("        ✦ •······················• ✦ •·�
 print()
 print("Your goal is to keep the kingdom running for as long as possible!")
 print("To do this, you'll have to manage four stats. If any of these stats reach 0 ... game over.")
+print()
+
+print("~Kingdom Stats~")
+print( f"   Treasury: {Treasury}")
+print( f"   Military: {Military}")
+print( f"   Food: {Food}")
+print( f"   Reputation: {Reputation}")
+
+situations = [
+    {
+        "person": "Farmer",
+        "text": "ঌ All my crops burned down!! Please give me more food to feed my family! ঌ",
+        "yes": {"food": -10, "reputation": 10},
+        "no": {"food": 10, "reputation": -10}
+    },
+
+    {
+        "person": "General",
+        "text": "We need to train more soldiers! If we don't, we will be vulnerable to attack.",
+        "yes": {"military": 10, "treasury": -10},
+        "no": {"military": -10, "treasury": 10}
+    }
+]
