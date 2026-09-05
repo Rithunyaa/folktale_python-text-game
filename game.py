bc4690958b904b6333de45e77a0921779223e134
BlaBlahBlahh <rithunyaa@gmail.com>
2026-09-05 23:32:46 +0000

    finished instructions

--- game.py
+++ game.py
@@ -5,8 +5,9 @@ Food = 50
 Reputation = 100
 
 #game title, the first few things that show up that tell you what is happening
-print("✦ •······················• ✦ •······················• ✦")
-print("           Your Majesty, We Have A Problem")
-print("✦ •······················• ✦ •······················• ✦")
+print("        ✦ •······················• ✦ •······················• ✦")
+print("                    Your Majesty, We Have A Problem")
+print("        ✦ •······················• ✦ •······················• ✦")
 print()
-print()
+print("Your goal is to keep the kingdom running for as long as possible!")
+print("To do this, you'll have to manage four stats. If any of these stats reach 0 ... game over.")
