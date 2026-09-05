8880cfa7c889e4de93d16eefc10be911d2251cc8
BlaBlahBlahh <rithunyaa@gmail.com>
2026-09-05 23:22:03 +0000

    finished heading

---
+++ game.py
@@ -0,0 +1,12 @@
+#stats that you start with
+Treasury = 100
+Military = 50
+Food = 50
+Reputation = 100
+
+#game title, the first few things that show up that tell you what is happening
+print("✦ •······················• ✦ •······················• ✦")
+print("           Your Majesty, We Have A Problem")
+print("✦ •······················• ✦ •······················• ✦")
+print()
+print()
